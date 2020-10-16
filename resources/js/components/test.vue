d4085cbb6a894fed5a8889ec5de1d9049c994829
lt970915 <1819783515@qq.com>
2020-10-16 14:51:42 +0800

    Initial commit

---
+++ resources/js/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Hello Component</div>
+
+                    <div class="card-body">
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloComponent'
+}
+</script>
+
+<style scoped>
+.container {
+    margin: 10px;
+}
+</style>
